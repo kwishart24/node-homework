565df50a5f099eeec81dfb2d8a6fa6d410becfb1
Kristen Wishart <kwishart24@gmail.com>
2026-03-29 18:05:34 +0900

    completed first 5 tasks of node core modules through fs.promises

--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,18 +1,38 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const fsp = require("fs/promises");
 
-const sampleFilesDir = path.join(__dirname, 'sample-files');
+const sampleFilesDir = path.join(__dirname, "sample-files");
 if (!fs.existsSync(sampleFilesDir)) {
   fs.mkdirSync(sampleFilesDir, { recursive: true });
 }
 
 // OS module
+const platform = os.platform();
+const cpu = os.cpus();
+const memory = os.totalmem();
 
-
+console.log("Platform: ", platform);
+console.log("CPU: ", cpu[0].model);
+console.log("Total Memory: ", memory);
 // Path module
-
+console.log("Joined path: ", sampleFilesDir);
 // fs.promises API
+const createNewFile = async () => {
+  try {
+    await fsp.writeFile("./sample-files/demo.txt", "Hello from fs.promises!");
 
+    const reading = await fsp.readFile("./sample-files/demo.txt", {
+      encoding: "utf-8",
+    });
+
+    console.log("fs.promises read: ", reading);
+  } catch (err) {
+    console.log("An error occurred.", err);
+  }
+};
+
+createNewFile();
 
 // Streams for large files- log first 40 chars of each chunk
