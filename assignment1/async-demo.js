302d2b84d49bac9dd9aa14c06af032e7227a5d5e
Kristen Wishart <kwishart24@gmail.com>
2026-03-29 16:26:25 +0900

    completed the callback and callback hell example in the async exercise

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,51 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+//const path = require("path");
 
 // Write a sample file for demonstration
 
 // 1. Callback style
 
+fs.readFile("./sample-files/sample.txt", { encoding: "utf8" }, (err, data) => {
+  if (err) {
+    console.log("file open failed: ", err.message);
+  } else {
+    //const string = data.toString();
+    console.log("Callback read: ", data);
+  }
+});
 
-  // Callback hell example (test and leave it in comments):
+// Callback hell example (test and leave it in comments):
 
+// //1st level: reading the file
+// fs.readFile("./sample-files/sample.txt", (err, data) => {
+//   if (err) {
+//     console.log("file open failed: ", err.message);
+//   } else {
+//     const string = data.toString();
+//     console.log("1st Callback read: ", string);
 
-  // 2. Promise style
+//     //2nd level: writing the file
+//     const content = "Hello from callback Hell";
+//     fs.writeFile("./sample-files/sample.txt", content, (err) => {
+//       if (err) {
+//         console.log("file rewrite failed: ", err.message);
+//       } else {
+//         console.log("2nd callback writes: ", content);
 
+//         //3rd level: reading the file
+//         fs.readFile("./sample-files/sample.txt", (err, data) => {
+//           if (err) {
+//             console.log("file open failed: ", err.message);
+//           } else {
+//             const string = data.toString();
+//             console.log("3rd Callback read: ", string);
+//           }
+//         });
+//       }
+//     });
+//   }
+// });
 
-      // 3. Async/Await style
+// 2. Promise style
+
+// 3. Async/Await style
