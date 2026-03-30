3b8d4ef8773aea9df642e0c4074953f4643e3809
Kristen Wishart <kwishart24@gmail.com>
2026-03-30 19:29:28 +0900

    fixed error status in not-found.js to pass all tests!

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -3,12 +3,11 @@ const { StatusCodes } = require("http-status-codes");
 const notFoundMiddleware = (req, res) => {
   console.error(
     `You can't do a ${req.method} for ${req.url}`,
-    JSON.stringify(["name", "message", "stack"]),
   );
 
   if (!res.headersSent) {
     return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .status(StatusCodes.NOT_FOUND)
       .send(`You can't do a ${req.method} for ${req.url}`);
   }
 };
