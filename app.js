d2f7da4b6d10bc5b6d1dcdf86cfda9dcd0149f2c
Kristen Wishart <kwishart24@gmail.com>
2026-03-30 19:21:20 +0900

    completed task 5 successfully

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path, req.query);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
