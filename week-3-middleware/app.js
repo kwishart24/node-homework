b14a36d747a3e57cc43c3c51e499e379dfcd9c39
Kristen Wishart <kwishart24@gmail.com>
2026-04-28 17:51:06 -0400

    added .vscode/ folder

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-const dogsRouter = require('./routes/dogs');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const dogsRouter = require("./routes/dogs");
 
 const app = express();
 
 // Your middleware here
 
-app.use('/', dogsRouter); // Do not remove this line
+app.use("/", dogsRouter); // Do not remove this line
 
-const server =	app.listen(3000, () => console.log("Server listening on port 3000"));
+const server = app.listen(3000, () =>
+  console.log("Server listening on port 3000"),
+);
 module.exports = server;
