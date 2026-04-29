91a18ceff3283d568927f826f6d0c91e36110b7b
Kristen Wishart <kwishart24@gmail.com>
2026-04-29 15:13:21 -0400

    added logon and logoff routes and controller. Still need to test

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,17 @@
 const { StatusCodes } = require("http-status-codes");
 
+// **************REGISTRATION*****************
 function register(req, res, body) {
   const { name, email, password } = req.body;
 
+  // Check that name, email, password were submitted
   if (!name || !email || !password) {
     return res
       .status(StatusCodes.BAD_REQUEST)
       .json({ error: "Name, email, and password are all required." });
   }
+
+  // Create new user from body of request if they were submitted
   const newUser = { ...req.body }; // this makes a copy
   global.users.push(newUser);
   global.user_id = newUser; // After the registration step, the user is set to logged on.
@@ -18,4 +22,52 @@ function register(req, res, body) {
     .json({ name: newUser.name, email: newUser.email });
 }
 
-module.exports = { register };
+// ************USER LOGON********************
+function logon(req, res, body) {
+  const { email, password } = req.body;
+
+  // Check that email and password were submitted
+  if (!email || !password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Email and password are required to logon." });
+  }
+
+  // Find user in system by email in global.users
+  const logonRequester = global.users.find((u) => u.email === email);
+
+  // If no email found, then return as unauthorized
+  if (logonRequester === undefined) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "Authenication Failed" });
+  }
+
+  // If user is found, then connect credentials entered with user found in the system
+  const foundUser = logonRequester;
+
+  const passwordFromReq = password;
+
+  // Compare passwords to make sure they match
+  if (foundUser.password !== passwordFromReq) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "Authentication Failed" });
+  } else {
+    // If successful, make the user logged in and the current user
+    global.user_id = foundUser.id;
+    return res
+      .status(StatusCodes.OK)
+      .json({ name: foundUser.name, email: foundUser.email });
+  }
+}
+
+// ************USER LOGOFF********************
+function logoff(req, res) {
+  global.user_id = null;
+  return res
+    .status(StatusCodes.OK)
+    .json({ message: "Logged off successfully." });
+}
+
+module.exports = { register, logon, logoff };
