095573c52ef655c2fb05179cfba32f8c560864c9
Kristen Wishart <kwishart24@gmail.com>
2026-04-29 13:07:17 -0400

    adding post request to app.js for task 1

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -5,7 +5,21 @@ const dogsRouter = require("./routes/dogs");
 
 const app = express();
 
+global.user_id = null;
+global.users = [];
+global.tasks = [];
+
 // Your middleware here
+app.use(express.json({ limit: "1kb" }));
+
+// Parsing the body of the request
+app.post("/api/users/register", (req, res) => {
+  const newUser = { ...req.body }; // this makes a copy
+  global.users.push(newUser);
+  global.user_id = newUser; // After the registration step, the user is set to logged on.
+  delete req.body.password;
+  res.status(201).json(req.body);
+});
 
 app.use("/", dogsRouter); // Do not remove this line
 
