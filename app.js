56965a278846bc37164fa0ff27609c5ce3c99b9f
Kristen Wishart <kwishart24@gmail.com>
2026-06-06 14:31:28 -0400

    task 1 complete, npm run tdd assignment10 tests passed

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
 app.use(helmet());
 
 // Express provided middleware which provides parsing
-app.use(express.json({ limit: "1kb" }));
+app.use(express.json({ limit: "1mb" }));
 
 //Cookie Parser
 app.use(cookieParser());
